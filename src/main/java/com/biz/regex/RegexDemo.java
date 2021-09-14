9edf19c2f1e38d33143d1b67e8fb85b70b019451
mkaubr007 <mkaubr007@gmail.com>
2021-09-15 00:55:36 +0530

    FirstName added

---
+++ src/main/java/com/biz/regex/RegexDemo.java
@@ -0,0 +1,25 @@
+package com.biz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo {
+    public static void main(String[] args) {
+        RegexDemo regexDemo=new RegexDemo();
+        regexDemo.firstName();
+    }
+    public void firstName(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first name");
+        String input=sc.next();
+        String regex="^[a-zA-Z]*$";
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(input);
+        if (matcher.matches()){
+            System.out.println("Valid");
+        }else {
+            System.out.println("Invalid");
+        }
+    }
+}
