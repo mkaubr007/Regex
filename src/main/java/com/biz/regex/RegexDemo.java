d5a55c22badb918a8942df10f08323c7e8db814b
mkaubr007 <mkaubr007@gmail.com>
2021-09-15 22:01:43 +0530

    FirstName added

--- src/main/java/com/biz/regex/RegexDemo.java
+++ src/main/java/com/biz/regex/RegexDemo.java
@@ -5,13 +5,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexDemo {
+    Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         RegexDemo regexDemo=new RegexDemo();
+        System.out.println("Enter first name");
         regexDemo.firstName();
     }
     public void firstName(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter first name");
         String input=sc.next();
         String regex="^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern=Pattern.compile(regex);
