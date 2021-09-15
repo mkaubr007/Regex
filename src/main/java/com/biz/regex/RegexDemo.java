36e4bc0b821c0c72e456c9694bc27a234ffef172
mkaubr007 <mkaubr007@gmail.com>
2021-09-15 23:12:50 +0530

    EmailValidation added

--- src/main/java/com/biz/regex/RegexDemo.java
+++ src/main/java/com/biz/regex/RegexDemo.java
@@ -5,17 +5,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexDemo {
+    Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         RegexDemo regexDemo=new RegexDemo();
+        System.out.println("Enter first name");
         regexDemo.firstName();
+        System.out.println("Enter last name");
         regexDemo.lastName();
+        System.out.println("Enter email");
         regexDemo.email();
+        System.out.println("Enter mobile number");
         regexDemo.phoneNumber();
+        System.out.println("Enter password");
         regexDemo.password();
     }
     public void firstName(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter first name");
         String input=sc.next();
         String regex="^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern=Pattern.compile(regex);
@@ -27,8 +31,6 @@ public class RegexDemo {
         }
     }
     public void lastName(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter last name");
         String input=sc.next();
         String regex="^[a-zA-Z]*$";
         Pattern pattern=Pattern.compile(regex);
@@ -40,8 +42,6 @@ public class RegexDemo {
         }
     }
     public void email(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter email");
         String input=sc.next();
         String regex="^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z]+.[a-z]{2,3}+.([a-z]{2,3})$";
         Pattern pattern=Pattern.compile(regex);
@@ -53,8 +53,6 @@ public class RegexDemo {
         }
     }
     public void phoneNumber(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter mobile number");
         String input=sc.next();
         String regex="(?:(?:\\+|0{0,2})91(\\s*[\\-]\\s*)?|[0]?)?[789]\\d{9}$";
         Pattern pattern=Pattern.compile(regex);
@@ -66,8 +64,6 @@ public class RegexDemo {
         }
     }
     public void password(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter password");
         String input=sc.next();
         String regex="^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$";
         Pattern pattern=Pattern.compile(regex);
